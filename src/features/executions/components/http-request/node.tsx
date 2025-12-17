6b237c4b8dfaff929ef077d92954d61f444a571d
adiya-deokar <adityadeokar80@gmail.com>
2025-12-17 23:30:12 +0530

    feat: refactor BaseNode and BaseExecutionNode components, add HttpRequestDialog for configuring HTTP requests

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -1,8 +1,10 @@
 "use client"
 
-import { Node, NodeProps } from "@xyflow/react";
+import { Node, NodeProps, useReactFlow } from "@xyflow/react";
 import BaseExecutionNode from "../base-execution-node";
 import {  GlobeIcon } from "lucide-react";
+import { useState } from "react";
+import HttpRequestDialog, { FormType } from "./dialog";
 
 type HttpRequestNodeData ={
     endpoint?: string;
@@ -17,20 +19,51 @@ type HttpRequestNodeType = Node<HttpRequestNodeData>
 
 const HttpRequestNode = (props: NodeProps<HttpRequestNodeType>) => {
 
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const{ setNodes }=useReactFlow()
+
+  const nodeStatus= "initial";
+
+  const handleOpenSettings = ()=> setDialogOpen(true);
+
+  const handleSubmit = (values:FormType)=>{
+    setNodes((nodes)=> nodes.map((node)=> {
+      if(node.id === props.id){
+        return {
+          ...node,
+          data: {
+              ...node.data,
+            endpoint: values.endpoint,
+            method:values.method,
+            body:values.body
+          }
+        } 
+      }
+
+      return node;
+    }))
+  }
+
     const nodeData = props.data;
     const description = nodeData?.endpoint ? `${nodeData.method || "GET"} : ${nodeData.endpoint}` : "Not Configured"
 
-
   return (
     <>
+        <HttpRequestDialog
+          onSubmit={handleSubmit}
+          defaultMethod={nodeData.method}
+          defaultBody={nodeData.body}
+          defualtEndpoint={nodeData.endpoint}
+          onOpenChange={setDialogOpen} open={dialogOpen}/>
          <BaseExecutionNode
             {...props}
             id={props.id}
             icon={GlobeIcon}
             name="HTTP Request"
             description={description}
-            onSettings={()=> {}}
-            onDoubleClick={()=> {}}
+            onSettings={handleOpenSettings}
+            onDoubleClick={handleOpenSettings}
+            status={nodeStatus}
          />
     </>
   )
